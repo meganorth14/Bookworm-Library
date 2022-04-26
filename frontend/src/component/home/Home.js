76190541288324ae425c4abf1fe68d0c5fcfe28c
ysabeau <kaila.1321@gmail.com>
2022-04-26 18:23:34 -0400

    started searchbar component

--- frontend/src/component/home/Home.js
+++ frontend/src/component/home/Home.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Footer from "../Footer";
 import Recommend from "./Recommended";
+import Searchbar from "./Searchbar";
+import "./home.css";
 
 function Home() {
   return(
     <>
+      <Searchbar />
       <Recommend />
       <Footer />
     </>
