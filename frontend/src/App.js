bd0d977aff34ff57164157959ebf7d3f03a4fbb4
ysabeau <kaila.1321@gmail.com>
2022-04-26 16:03:11 -0400

    made a basic footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./component/style.css";
 
 import Home from './component/home/Home';
 import LoginRegisterPage from './component/loginregister/LoginRegisterPage';
--- frontend/src/component/Footer.js
+++ frontend/src/component/Footer.js
@@ -1,9 +1,41 @@
 import React from "react";
+import { Telephone, EnvelopeHeart, Facebook, Twitter, Github } from "react-bootstrap-icons";
+
 
 function Footer() {
   return(
     <>
-
+      <footer className="site-footer">
+        <div className="container">
+          <hr></hr>
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 col-sm-6 col-xs-12">
+              <p className="copyright-text">Bookworm Library &copy; 2022 All Rights Reserved.</p>
+            </div>
+            <div className="col-md-4 col-sm-6 col-xs-12">
+              <ul className="social-icons">
+                <li>
+                  <a className="facebook" href="/">
+                    <Facebook size={30} color="#fff" />
+                  </a>
+                </li>
+                <li>
+                  <a className="twitter" href="/">
+                    <Twitter size={30} color="#fff" />
+                  </a>
+                </li>
+                <li>
+                  <a className="github" href="/">
+                    <Github size={30} color="#fff" />
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </footer>
     </>
   );
 }
