4b22e93df59a7b0e28f12f07d5b3d3a5f92522de
gtwnsnd20 <gtwnsnd20@gmail.com>
2022-04-30 16:19:53 -0500

    admin components initial commit

--- frontend/src/component/admin/ViewAuthors.js
+++ frontend/src/component/admin/ViewAuthors.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Button, Card, Table } from "react-bootstrap";
 
-const genres = [
-  { genreId: 1, name: 'Historical Fiction' },
-  { genreId: 2, name: 'High Fantasy' },
-  { genreId: 3, name: 'Drama' },
-  { genreId: 4, name: 'Thriller' }
+const authors = [
+  { authorId: 1, name: 'Jules Verne' },
+  { authorId: 2, name: 'F. Scott Fitzgerald' },
+  { authorId: 3, name: 'Aristotle' },
+  { authorId: 4, name: 'G.R.R. Martin' }
 ];
 
-function ViewGenres() {
+function ViewAuthors() {
   return (
     <>
       <Card style={{ marginTop: '10px' }}>
@@ -16,15 +16,15 @@ function ViewGenres() {
           <Table striped bordered hover responsive >
             <thead>
               <tr>
-                <th>Genre Name</th>
-                <th>Delete Genre</th>
+                <th>Author Name</th>
+                <th>Delete Author</th>
               </tr>
             </thead>
             <tbody>
               {/* Dynamically Render Users START */}
-              {genres.map((genre) => (
-                <tr key={genre.genreId}>
-                  <td>{genre.name}</td>
+              {authors.map((author) => (
+                <tr key={author.authorId}>
+                  <td>{author.name}</td>
                   <td>
                     <Button disabled>Delete</Button>
                   </td>
@@ -39,4 +39,4 @@ function ViewGenres() {
   );
 }
 
-export default ViewGenres;
+export default ViewAuthors;
