0938d3aea18a96796108495fea95f2c537207a32
meganorth14 <meganorth14@gmail.com>
2022-05-05 12:19:51 -0700

    updated orderRepo, removed order from user

--- backend/bookwormlibrary/src/main/java/com/revature/bookwormlibrary/dao/OrderRepository.java
+++ backend/bookwormlibrary/src/main/java/com/revature/bookwormlibrary/dao/OrderRepository.java
@@ -2,6 +2,7 @@ package com.revature.bookwormlibrary.dao;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 //import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -10,6 +11,6 @@ import com.revature.bookwormlibrary.entity.Order;
 
 @Repository
 public interface OrderRepository extends CrudRepository<Order, Integer> {
-//    @Query("SELECT o FROM Order o WHERE o.user.user_id = ?1")
+    @Query("SELECT o FROM Order o WHERE o.user = ?1")
     List<Order> getOrderByUserid(Integer id);
 }
