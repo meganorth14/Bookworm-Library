064bf0153c08f7ac0b617b0e90b8494eaf2f8a09
gtwnsnd20 <gtwnsnd20@gmail.com>
2022-05-06 16:20:49 -0500

    added find methods

--- backend/bookwormlibrary/src/main/java/com/revature/bookwormlibrary/dao/AuthorRepository.java
+++ backend/bookwormlibrary/src/main/java/com/revature/bookwormlibrary/dao/AuthorRepository.java
@@ -1,5 +1,7 @@
 package com.revature.bookwormlibrary.dao;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.revature.bookwormlibrary.entity.Author;
 
 @Repository
 public interface AuthorRepository extends CrudRepository<Author, Integer> {
-
+	Optional<Author> findByName(String name);
 }
--- backend/bookwormlibrary/src/main/java/com/revature/bookwormlibrary/dao/GenreRepository.java
+++ backend/bookwormlibrary/src/main/java/com/revature/bookwormlibrary/dao/GenreRepository.java
@@ -1,5 +1,7 @@
 package com.revature.bookwormlibrary.dao;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.revature.bookwormlibrary.entity.Genre;
 
 @Repository
 public interface GenreRepository extends CrudRepository<Genre, Integer> {
-
+	Optional<Genre> findByName(String name);
 }
