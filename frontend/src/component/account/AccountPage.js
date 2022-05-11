5a26c1d1b51aab9e78cc6c202249d8926478267e
gtwnsnd20 <gtwnsnd20@gmail.com>
2022-05-11 01:58:15 -0500

    moved date/TimeToString to HelperMethods

--- frontend/src/component/account/AccountPage.js
+++ frontend/src/component/account/AccountPage.js
@@ -1,72 +1,44 @@
 import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { Container, Row, Col, Card } from 'react-bootstrap';
-import { RiPencilLine } from "react-icons/ri";
 import axios from 'axios';
 import { useSelector } from 'react-redux';
+import { dateToString, dateTimeToString } from '../utility/HelperMethods';
 
-const AccountPage = () => {
-    const userid = useSelector((state) => state.login.value.userid);
+function AccountPage() {
 
-    const [profile, setProfile] = useState({});
+  const userid = useSelector((state) => state.login.value.userid);
 
-    useEffect(() => {
-        axios.get(`http://localhost:8080/userbyid/${userid}`).then((res) => {
-            res.data.registrationDate = dateToString(res.data.registrationDate);
-            res.data.lastLogin = dateTimeToString(res.data.lastLogin);
-            setProfile(res.data);
-        });
-    }, []);
+  const [profile, setProfile] = useState({});
 
-    const dateToString = (date) => {
-        date = date.map(dt => dt>10 ? dt:`0${dt}`);
-        const [year,month,day] =  date;
-        return `${month}/${day}/${year}`;
-    }
-    const dateTimeToString = (dateTime) => {
-    dateTime = dateTime.map(dt => dt>10 ? dt:`0${dt}`);
-    let [year,month,day,hour,min,sec] = dateTime;
-    return `${month}/${day}/${year} ${hour}:${min}:${sec}`;
-    }  
+  useEffect(() => {
+    axios.get(`http://localhost:8080/userbyid/${userid}`).then((res) => {
+      res.data.registrationDate = dateToString(res.data.registrationDate);
+      res.data.lastLogin = dateTimeToString(res.data.lastLogin);
+      setProfile(res.data);
+    });
+  }, [userid]);
 
-    let navigate = useNavigate();
-
-    return (
-        <Container className="account-page">
-            <Row className="justify-content-md-center mb-4">
-                <Col md={6}>
-                    <Card >
-                        <Card.Header as="h5" bsPrefix="account-header">
-                            Account Details
-                        </Card.Header>
-                        <Card.Body className='userbody'>
-                            <Row>
-                                <p><b>First name:</b> {profile.firstName}</p>
-                            </Row>
-
-                            <Row>
-                                <p><b>Last name:</b> {profile.lastName}</p>
-                            </Row>
-                            <Row>
-                                <p><b>Username:</b> {profile.username}</p>
-                            </Row>
-                            <Row>
-                                <p><b>Email:</b> {profile.email}</p>
-                            </Row>
-                            <Row>
-                                <p><b>Registration date:</b> {profile.registrationDate}</p>
-                            </Row>
-                            <Row>
-                                <p><b>Last login:</b> {profile.lastLogin}</p>
-                            </Row>
-                        </Card.Body>
-                    </Card>
-                </Col>
-
-            </Row>
-        </Container>
-
-    );
-};
+  return (
+    <Container className="account-page">
+      <Row className="justify-content-md-center mb-4">
+        <Col md={5}>
+          <Card>
+            <Card.Header as="h5" bsPrefix="account-header">
+              Account Details
+            </Card.Header>
+            <Card.Body className='userbody'>
+              <Card.Text><b>Name:</b> {profile.firstName} {profile.lastName}</Card.Text>
+              <Card.Text><b>Username:</b> {profile.username}</Card.Text>
+              <Card.Text><b>Email:</b> {profile.email}</Card.Text>
+              <Card.Text><b>Registration Date:</b> {profile.registrationDate}</Card.Text>
+              <Card.Text><b>Last Login:</b> {profile.lastLogin}</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+  
+}
 
 export default AccountPage;
