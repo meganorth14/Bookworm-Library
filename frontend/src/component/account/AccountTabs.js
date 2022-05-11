0de8d16e20e916713b40f8f002f8af53edc662c8
gtwnsnd20 <gtwnsnd20@gmail.com>
2022-05-11 01:59:21 -0500

    changed EditAccount to AccountForm

--- frontend/src/component/account/AccountTabs.js
+++ frontend/src/component/account/AccountTabs.js
@@ -2,7 +2,7 @@ import React, { useState, } from 'react';
 import {Container, Tabs, Tab} from "react-bootstrap";
 import OrderHistory from "./OrderHistory";
 import AccountPage from './AccountPage';
-import EditAccount from './EditAccount';
+import AccountForm from './AccountForm';
 
 function AccountTabs() {
 
@@ -15,7 +15,7 @@ function AccountTabs() {
                     <AccountPage />
                 </Tab>
                 <Tab eventKey='edit' title='Edit Account' tabClassName="tabs">
-                    <EditAccount setKey={setKey}/>
+                    <AccountForm setKey={setKey}/>
                 </Tab>
                 <Tab eventKey='orders' title='Order History' tabClassName="tabs">
                     <OrderHistory />
@@ -23,6 +23,6 @@ function AccountTabs() {
             </Tabs>
         </Container>
     );
-};
+}
 
 export default AccountTabs;
